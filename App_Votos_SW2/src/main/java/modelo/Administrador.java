df17882e0c7020930fd4804ec1fccd40f57102f4
maralvarig <malvai00@estudiantes.unileon.es>
2022-05-12 17:15:58 +0200

    Mapeado 2.1

--- App_Votos_SW2/src/main/java/modelo/Administrador.java
+++ App_Votos_SW2/src/main/java/modelo/Administrador.java
@@ -5,10 +5,55 @@
  */
 package modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author dnarc
  */
-public class Administrador {
+@Entity
+@Table(name="administrador")
+public class Administrador implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public int idAdministrador;
+    
+    @Column(name="Usuario")
+    private String Usuario;
+    
+    @Column(name="Contrasenya")
+    private String Contrasenya;
+    
+    
+    public int getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public void setIdAdministrador(int idAdministrador) {
+        this.idAdministrador = idAdministrador;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getContrasenya() {
+        return Contrasenya;
+    }
+
+    public void setContrasenya(String Contrasenya) {
+        this.Contrasenya = Contrasenya;
+    }
     
 }
