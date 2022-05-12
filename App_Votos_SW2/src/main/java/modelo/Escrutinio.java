e83e593ef2f0761dfe6d92e77dcba491926205da
maralvarig <malvai00@estudiantes.unileon.es>
2022-05-12 17:10:50 +0200

    Mapeado 2

--- App_Votos_SW2/src/main/java/modelo/Escrutinio.java
+++ App_Votos_SW2/src/main/java/modelo/Escrutinio.java
@@ -5,10 +5,68 @@
  */
 package modelo;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author dnarc
  */
-public class Escrutinio {
+@Entity
+@Table(name="Escrutinio")
+public class Escrutinio implements Serializable{
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idEscrutinio;
+    
+    @Column(name="Resultados")
+    private String Resultados;
+    
+    @JoinColumn(name="Elecciones_idElecciones")
+    @ManyToOne
+    private Elecciones Elecciones_idElecciones;
+
+    @JoinColumn(name="Elecciones_Localidad_idLocalidad")
+    @ManyToOne
+    private Localidad Elecciones_Localidad_idLocalidad;
+
+    public int getIdEscrutinio() {
+        return idEscrutinio;
+    }
+
+    public void setIdEscrutinio(int idEscrutinio) {
+        this.idEscrutinio = idEscrutinio;
+    }
+
+    public String getResultados() {
+        return Resultados;
+    }
+
+    public void setResultados(String Resultados) {
+        this.Resultados = Resultados;
+    }
+
+    public Elecciones getElecciones_idElecciones() {
+        return Elecciones_idElecciones;
+    }
+
+    public void setElecciones_idElecciones(Elecciones Elecciones_idElecciones) {
+        this.Elecciones_idElecciones = Elecciones_idElecciones;
+    }
+
+    public Localidad getElecciones_Localidad_idLocalidad() {
+        return Elecciones_Localidad_idLocalidad;
+    }
+
+    public void setElecciones_Localidad_idLocalidad(Localidad Elecciones_Localidad_idLocalidad) {
+        this.Elecciones_Localidad_idLocalidad = Elecciones_Localidad_idLocalidad;
+    }
     
 }
--- App_Votos_SW2/src/main/java/modelo/Localidad.java
+++ App_Votos_SW2/src/main/java/modelo/Localidad.java
@@ -5,10 +5,39 @@
  */
 package modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author dnarc
  */
+@Entity
+@Table(name="Localidad")
 public class Localidad {
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idEscrutinio;
+    
+    @Column(name="Pais")
+    private String Pais;
+    
+    @Column(name="Comunidad_Autonoma")
+    private String Comunidad_Autonoma;
+    
+    @Column(name="Provincia")
+    private String Provincia;
+    
+    @Column(name="Municipio")
+    private String Municipio;
+    
+    @Column(name="Codigo_Postal")
+    private String Codigo_Postal;
     
 }
