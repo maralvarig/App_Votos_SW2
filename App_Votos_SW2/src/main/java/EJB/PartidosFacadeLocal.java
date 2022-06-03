ea9af522b3c70699226949c9db8ee6a3d6674d2a
dnarcr00 <dnarcr00@estudiantes.unileon.es>
2022-06-03 17:50:20 +0200

    Solucion Problemas v2

--- App_Votos_SW2/src/main/java/EJB/PartidosFacadeLocal.java
+++ App_Votos_SW2/src/main/java/EJB/PartidosFacadeLocal.java
@@ -7,6 +7,7 @@ package EJB;
 
 import java.util.List;
 import javax.ejb.Local;
+import modelo.Elecciones;
 import modelo.Partidos;
 
 /**
@@ -30,4 +31,5 @@ public interface PartidosFacadeLocal {
 
     int count();
     
+    List<Partidos> encontrarPartidos(Elecciones eleccion);
 }
